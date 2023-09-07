515bfb67467c4e172fae7526bdf5f199a311d43e
Ewan Sheldon <ewansheldon@gmail.com>
2023-09-07 11:48:26 +0200

    health check response

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const http = require('http');
 require('dotenv').config();
 const { schedulePricingIndexing } = require('./src/pricing');
 
+const port = process.env.PORT || 3000;
+
 schedulePricingIndexing();
+
+const server = http.createServer(async (req, res) => {
+  console.log(`${new Date().toISOString()} | ${req.method} ${req.url}`);
+  res.statusCode = 200;
+  res.end();
+});
+
+server.listen(port);
+console.log(`Smart Vault Job Runner running on port ${port}`)
