f7d08e2bf853ca17bc086a7e9cb30991d0db60f8
Ewan Sheldon <ewansheldon@gmail.com>
2023-09-12 14:44:35 +0200

    index yield opportunities

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const { schedulePricingIndexing } = require('./src/pricing');
 const { scheduleStatIndexing } = require('./src/stats');
 const { scheduleLiquidation } = require('./src/liquidation');
+const { scheduleIndexYieldData } = require('./src/yield');
 
 const port = process.env.PORT || 3000;
 
 schedulePricingIndexing();
 scheduleStatIndexing();
 scheduleLiquidation();
+scheduleIndexYieldData();
 
 const server = http.createServer(async (req, res) => {
   res.statusCode = 200;
