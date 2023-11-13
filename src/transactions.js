a8d12fb66089a0c61930b39a2f110b676571f30e
Ewan Sheldon <ewansheldon@gmail.com>
2023-11-13 16:27:55 +0100

    index txs every 5 mins

--- src/transactions.js
+++ src/transactions.js
@@ -434,7 +434,7 @@ const indexVaultTransactions = async _ => {
 
 const scheduleVaultTransactionIndexing = async _ => {
   let running = false;
-  schedule.scheduleJob('* * * * *', async _ => {
+  schedule.scheduleJob('*/5 * * * *', async _ => {
     if (!running) {
       running = true;
       await indexVaultTransactions();
