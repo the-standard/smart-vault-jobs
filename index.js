8cd0779201c955752177378eddf67d1623c55199
Ewan Sheldon <ewansheldon@gmail.com>
2024-06-24 11:41:56 +0200

    remove liquidation pool data

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const http = require('http');
 require('dotenv').config();
 const { schedulePricingIndexing } = require('./src/pricing');
-const { scheduleStatIndexing } = require('./src/stats');
 const { scheduleIndexYieldData } = require('./src/yield');
 const { scheduleVaultTransactionIndexing } = require('./src/transactions');
-const { scheduleLiquidationPoolData } = require('./src/liquidationPools');
 
 const port = process.env.PORT || 3000;
 
 schedulePricingIndexing();
-// scheduleStatIndexing();
 scheduleIndexYieldData();
 scheduleVaultTransactionIndexing();
-scheduleLiquidationPoolData();
 
 const server = http.createServer(async (req, res) => {
   res.statusCode = 200;
