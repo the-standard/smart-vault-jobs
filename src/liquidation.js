66e4874f1057ea61d2fd7cce6495e918769d350f
Ewan Sheldon <ewansheldon@gmail.com>
2025-03-13 17:11:42 +0100

    liquidation check every 4 hours

--- src/liquidation.js
+++ src/liquidation.js
@@ -105,8 +105,7 @@ const scheduleLiquidation = async _ => {
   const network = getNetwork('arbitrum');
 
   // posts liquidation info to discord
-  // schedule.scheduleJob('38 */4 * * *', async _ => {
-  schedule.scheduleJob('38 * * * *', async _ => {
+  schedule.scheduleJob('38 */4 * * *', async _ => {
     console.log('logging liquidation info');
     const provider = new ethers.getDefaultProvider(network.rpc);
     const wallet = new ethers.Wallet(process.env.WALLET_PRIVATE_KEY, provider);
